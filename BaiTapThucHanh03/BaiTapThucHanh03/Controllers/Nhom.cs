0890ef6ba869ad3be9af0a6481541ed151146de0
Nguyen Huy Hoang <nhhoang454@gmail.com>
2024-08-27 14:52:03 +0700

    add Bai tap 03 ngay 27/8/2024

---
+++ BaiTapThucHanh03/BaiTapThucHanh03/Controllers/Nhom.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapThucHanh03.Controllers
+{
+    public class Nhom : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
