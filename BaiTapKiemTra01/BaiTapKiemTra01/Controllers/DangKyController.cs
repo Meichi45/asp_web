73eb75812b2d4b3ca12e297652233e74d02d8353
Nguyen Huy Hoang <nhhoang454@gmail.com>
2024-09-10 15:14:43 +0700

    Bai Tap 01

---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Controllers/DangKyController.cs
@@ -0,0 +1,17 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class DangKyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel model)
+        { 
+            return View(model);
+        }
+    }
+}
