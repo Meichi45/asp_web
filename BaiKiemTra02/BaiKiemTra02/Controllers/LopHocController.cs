f3821e426c8f02d28f2a45c6e8f051fa43096c52
Nguyen Huy Hoang <nhhoang454@gmail.com>
2024-09-24 15:14:56 +0700

    Nop bai Kiemtra02

---
+++ BaiKiemTra02/BaiKiemTra02/Controllers/LopHocController.cs
@@ -0,0 +1,41 @@
+﻿using BaiKiemTra02.Data;
+using BaiKiemTra02.Data.Migrations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiKiemTra02.Controllers
+{
+    public class LopHocController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public LopHocController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var lophoc= _db.LopHoc.ToList();
+            ViewBag.Lophoc = lophoc;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(LopHoc lophoc)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                _db.LopHoc.Add(lophoc);
+                
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+    }
+}
---
+++ BaiKiemTra02/BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BaiKiemTra02.Models
+{
+    public class LopHoc
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string TenLopHoc {  get; set; }
+        [Required]
+        public DateTime NamNhapHoc { get; set; }
+        [Required]
+        public DateTime NamRaTruong { get; set; }
+        [Required]
+        public int SoLuongSinhVien { get; set; }
+    }
+}
